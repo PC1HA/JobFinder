c722ca71c40285d7d81c4cdf1a53ef612fac0c37
PC1HA <ilia.popov.2015@gmail.com>
2025-04-21 00:37:12 +0300

    v0.2 созданы основные функции

---
+++ main.py
@@ -0,0 +1,45 @@
+from api.hh_api import HeadHunterAPI
+from models.vacancy import Vacancy
+from storage.json_storage import JSONStorage
+from utils.helpers import filter_vacancies, sort_vacancies, get_top_vacancies
+
+
+def user_interaction() -> None:
+    """Основная функция взаимодействия с пользователем."""
+
+    hh_api = HeadHunterAPI()
+
+    search_query = input("Введите поисковый запрос: ")
+
+    # Получение вакансий с hh.ru в формате JSON и преобразование в объекты Vacancy.
+    vacancies_data = hh_api.get_vacancies(search_query)
+
+    vacancies_list = [
+        Vacancy(
+            v['name'],
+            v['alternate_url'],
+            v.get('salary', {}).get('from', 0),
+            v['snippet']['requirement']
+        ) for v in vacancies_data
+    ]
+
+    # Сохранение вакансий в файл.
+
+
+json_saver = JSONStorage()
+
+for vacancy in vacancies_list:
+    json_saver.add_vacancy(vacancy)
+
+top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+
+sorted_vacancies = sort_vacancies(vacancies_list)
+
+top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
+
+print("Топ вакансий:")
+for vacancy in top_vacancies:
+    print(vacancy)
+
+if __name__ == "__main__":
+    user_interaction()
